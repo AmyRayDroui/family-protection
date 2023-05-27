0281b84846a87e4e090718c03058de268b177cc0
AmyRay <amyraydroui@gmail.com>
2023-05-28 00:56:04 +0300

    fix banner images style

--- components/home-components/banner.tsx
+++ components/home-components/banner.tsx
@@ -3,6 +3,7 @@ import themePreval from '@/lib/theme.preval'
 import React from 'react'
 import { PageHeader } from '../data-components/header-text'
 import { FullButton } from '../data-components/full-button'
+import { getBannerClassName } from '@/lib/utils'
 
 export default function Banner({ data }: HomePageType) {
   return (
@@ -23,7 +24,9 @@ export default function Banner({ data }: HomePageType) {
           <img
             src={image?.url}
             alt={image?.alt}
-            className={image?.customData?.className}
+            className={`banner-img sm:hidden rounded-lg ${getBannerClassName(
+              image?.alt,
+            )}`}
             key={index}
           />
         ))}
@@ -50,6 +53,57 @@ export default function Banner({ data }: HomePageType) {
             position: absolute;
             width: 15%;
             min-width: 200px;
+            border: white solid 2px;
+          }
+          .handsHolding {
+            top: 300px;
+            left: -8px;
+            z-index: 1;
+          }
+          .hands {
+            top: 0;
+            left: 637px;
+          }
+          .granny {
+            top: 259px;
+            right: -8px;
+            z-index: 1;
+          }
+          .father {
+            top: 60px;
+            right: 150px;
+            z-index: 0;
+          }
+          .familyField {
+            top: 0;
+            left: 165px;
+            z-index: 0;
+          }
+          .twoWomen {
+            top: 540px;
+            right: 343px;
+          }
+          .pregnantWoman {
+            top: 540px;
+            left: 322px;
+            z-index: 0;
+          }
+          @media (max-width: 1500px) {
+            .hands {
+              left: 500px;
+            }
+            .father {
+              right: 15px;
+            }
+            .familyField {
+              left: 15px;
+            }
+            .twoWomen {
+              right: 200px;
+            }
+            .pregnantWoman {
+              left: 200px;
+            }
           }
         `}</style>
       </div>
---
+++ lib/utils.ts
@@ -0,0 +1,24 @@
+export function getBannerClassName(name: string) {
+  switch (name) {
+    case 'hands holding':
+      return 'handsHolding'
+
+    case 'hands':
+      return 'hands'
+
+    case 'granny':
+      return 'granny'
+
+    case 'father':
+      return 'father'
+
+    case 'family field':
+      return 'familyField'
+
+    case 'two women':
+      return 'twoWomen'
+
+    case 'pregnant woman':
+      return 'pregnantWoman'
+  }
+}
